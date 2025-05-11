f1dd1d00f4dd484ca2ca05a876a172ed4530f81a
shanumas <shanumas@gmail.com>
2025-05-11 21:11:49 +0200

    single quotes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,28 +9,28 @@ import { footerVariants } from '../utils/motion';
 const Footer = () => (
   <motion.footer
     variants={footerVariants}
-    initial='hidden'
-    whileInView='show'
+    initial="hidden"
+    whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div className='footer-gradient' />
+    <div className="footer-gradient" />
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-      <div className='flex items-center justify-between flex-wrap gap-5'>
-        <h4 className='font-bold md:text-[64px] text-[44px] text-white'>
+      <div className="flex items-center justify-between flex-wrap gap-5">
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
           Call to book an appointment
         </h4>
         <button
-          type='button'
-          className='flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]'
+          type="button"
+          className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]"
         >
           <img
-            src='/headset.svg'
-            alt='headset'
-            className='w-[24px] h-[24px] object-contain'
+            src="/headset.svg"
+            alt="headset"
+            className="w-[24px] h-[24px] object-contain"
           />
-          <span className='font-normal text-[16px] text-white'>
+          <span className="font-normal text-[16px] text-white">
             <a
-              href='tel:0762739173'
+              href="tel:0762739173"
               style={{
                 display: 'block',
                 padding: '8px 0',
@@ -44,25 +44,25 @@ const Footer = () => (
         </button>
       </div>
 
-      <div className='flex flex-col'>
-        <div className='mb-[50px] h-[2px] bg-white opacity-10' />
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
-        <div className='flex items-center justify-between flex-wrap gap-4'>
-          <h4 className='font-extrabold text-[24px] text-white'>
+        <div className="flex items-center justify-between flex-wrap gap-4">
+          <h4 className="font-extrabold text-[24px] text-white">
             NILA - Beauty Parlour - Gothenburg
           </h4>
-          <p className='font-normal text-[14px] text-white opacity-50'>
+          <p className="font-normal text-[14px] text-white opacity-50">
             Copyright © 2025 - NILA. All rights reserved.
           </p>
 
-          <div className='flex gap-4 text-white'>
+          <div className="flex gap-4 text-white">
             Instagram:
             {socials.map((social) => (
               <a key={social.name} href={social.link}>
                 <img
                   src={social.url}
                   alt={social.name}
-                  className='w-[24px] h-[24px] object-contain'
+                  className="w-[24px] h-[24px] object-contain"
                 />
               </a>
             ))}
